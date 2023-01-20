3d727b186313f6ff80c3e8cc94d48ffcf2e4e395
lumo03 <luis.moncada.mso@gmail.com>
2023-01-20 12:02:21 +0100

    first version

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                stream.write_all(&read[0..n]).unwrap();
+            }
+            Err(err) => panic!("Error: {}", err),
+        }
+    }
+}
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(_) => panic!("An error occured."),
+        }
+    }
+}
